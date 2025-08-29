8de44a7fd2a2ea3b44fd92949df99e966172638b
Rodolfo Ramírez <rramirezc@uamv.edu.ni>
2025-08-29 01:29:26 -0600

    Add files via upload
    Ejemplos parte 1

---
+++ C#/Semana_1/Ejemplos/if/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Escribe tu edad actual: ");
+        int edad = int.Parse(Console.ReadLine());
+        
+        if (edad >= 18)
+        {
+            Console.WriteLine("Eres mayor de edad.");
+        }
+    }
+}
+
