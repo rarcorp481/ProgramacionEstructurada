b2c8eb0872ff93566d8485290d1653d299d7e3bf
Rodolfo Ramírez <rramirezc@uamv.edu.ni>
2025-08-29 01:24:41 -0600

    Add files via upload
    Ejercicios del 1 al 2 más el de ejemplo

---
+++ C#/Semana_1/Ejercicios/Ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Programa para calcular el área total en manzanas de un terreno en una finca nicaragüense, donde el usuario ingresa 
+// la longitud y el ancho en varas (1 manzana = 1,428.8 varas²) utilizando  una cantidad fija de 5 varas de ancho 
+// y la longitud como entrada.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        const double VarasCuadradasPorManzana = 1428.8;
+        const double AnchoTerreno = 5.0; // Le puse const para definir que son constantes en el programa aunque realmente no sea necesario en este caso por lo corto qeu es el ejecicio
+
+        Console.WriteLine("\nPrograma para calcular el área total en manzanas de un terreno rectangular de una finca nicaragüense.");
+        Console.Write("Ingrese la longitud del terreno en Varas: ");
+        if (!double.TryParse(Console.ReadLine(), out double LargoTerreno) || LargoTerreno <= 0)
+        {
+            Console.WriteLine("Ingrese una longitud válida. Intente de nuevo por favor.");
+            return;
+        }
+
+        Console.WriteLine("\nCalculando...");
+         double TotalManzanas = AnchoTerreno * LargoTerreno / VarasCuadradasPorManzana;
+        Console.WriteLine($"El área total del terreno de {AnchoTerreno} vars de ancho y {LargoTerreno} vars de largo es de {TotalManzanas:F4} manzanas.");
+
+    }
+}
