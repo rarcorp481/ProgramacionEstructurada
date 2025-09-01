514590828e37dafddf626a7b5c8ea653354f6d28
Rodolfo Ramírez <rramirezc@uamv.edu.ni>
2025-09-01 01:32:47 -0600

    feat: Añadir carpeta de la semana 1
    Se añadió la carpeta de la semana 1 con la subcarpeta de practicasIfSwitchCS con el primer ejercicio.

---
+++ C#/semana1/practicasIfSwitchCS/1-NumeroPositivoNegativoCero/Program.cs
@@ -0,0 +1,32 @@
+﻿// Determinar si un número ingresado es posiitivo, negativo o cero.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== ¿ES POSITIVO, NEGATIVO O CERO? ===");
+        Console.Write("Ingrese un número: ");
+        string? entrada = Console.ReadLine();
+
+        if (!double.TryParse(entrada, out double numero))
+        {
+            Console.WriteLine("\nEntrada inválida. Por favor, ingrese un número válido.");
+        }
+        else if (Convert.ToDouble(entrada) > 0)
+        {
+            Console.WriteLine("\nEl número es positivo.");
+        }
+        else if (Convert.ToDouble(entrada) < 0)
+        {
+            Console.WriteLine("\nEl número es negativo.");
+        }
+        else
+        {
+            Console.WriteLine("\nEl número es cero.");
+        }
+
+        Console.Write("\nPresiona enter para salir: ");
+        Console.ReadLine();
+    }
+}
