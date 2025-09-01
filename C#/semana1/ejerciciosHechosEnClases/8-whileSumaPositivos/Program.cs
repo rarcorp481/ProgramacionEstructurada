87318342c76f9347a9e015fbc92d217f0618375d
Rodolfo Ramírez <rramirezc@uamv.edu.ni>
2025-09-01 02:02:20 -0600

    feat: excercises 7 & 8 added to ejerciciosHechosEnClases

---
+++ C#/semana1/ejerciciosHechosEnClases/8-whileSumaPositivos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+       static void Main()
+    {
+        Console.WriteLine("Programa para aprender a utilizar la estructura while");
+        int num, suma = 0; 
+        Console. Write("Ingrese un número positivo (o un número negativo para terminar el bucle While): ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        while (num > 0)
+        {
+            suma += num;
+            Console.Write("Ingrese un número positivo (o un número negativo para terminar el bucle While): ");
+            num = Convert.ToInt32(Console.ReadLine());
+            
+        }
+        Console.WriteLine("La suma de los números positivos es: " + suma);
+        Console.WriteLine("\nPresione Enter para salir...");
+        Console.ReadLine();
+    }
+}
