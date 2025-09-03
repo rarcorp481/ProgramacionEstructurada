c26a8932b45ebf327f97f48ff33c33aa6115b1fa
Rodolfo Ramírez <rramirezc@uamv.edu.ni>
2025-09-02 23:59:07 -0600

    add: Ejercicios semana 2 / ejercicio 1-2

---
+++ C#/semana2/ejerciciosS1/1-repartoGastosGira/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+class  Program 
+{
+    static void Main(string[] args) 
+    {
+        decimal gastoTotal;
+        int numeroParticipantes;
+
+        Console.WriteLine("=== PROGRAMA PARA CALCULAR REPARTO DE GASTOS EN UNA GIRA ===");
+        Console.Write("\nIngrese el gasto total de la gira en $: ");
+        string? entrada = Console.ReadLine();
+
+        // bucle para validar la entrada del gasto total de la gira
+        while (!decimal.TryParse(entrada, out gastoTotal) || gastoTotal < 0) 
+        {
+            Console.Write("Entrada inválida. Por favor, ingrese un número válido para el gasto total: ");
+            entrada = Console.ReadLine();
+        }
+        Console.Write("\nIngrese el número de participantes en la gira: ");
+        entrada = Console.ReadLine();
+
+        // bucle para validar la entrada del número de participantes
+        while (!int.TryParse(entrada, out numeroParticipantes) || numeroParticipantes < 0) // se pudo haber puesto <= 0 pero se maneja la excepción de división por cero en el bloque try-catch como se pide en el enunciado
+
+        {
+            Console.Write("Entrada inválida. Por favor, ingrese un número válido para el número de participantes: ");
+            entrada = Console.ReadLine();
+        }
+
+        //bucle para manejar la excepción de división por cero
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine($"\nTotal: {gastoTotal}$\nParticipantes: {numeroParticipantes}\nCada participante debe pagar: {gastoTotal / numeroParticipantes:F2}$");
+                break;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.Write("\nError: El número de participantes no puede ser cero. Reintenta para ingresar un número válido: ");
+                string? entrada2 = Console.ReadLine();
+
+                if (!int.TryParse(entrada2, out numeroParticipantes) || numeroParticipantes <= 0)  // Forzar al usuario a ingresar un número válido repitiendo el ciclo hasta que lo haga
+                {
+                    continue; // sigue con el ciclo hasta que entrada2 sea mayor que 0
+                }
+                else
+                {
+                    numeroParticipantes = int.Parse(entrada2);
+                }
+            }
+        }
+            
+    }
+
+}
---
+++ C#/semana2/ejerciciosS1/2-facturaFerreteria/Program.cs
@@ -0,0 +1,86 @@
+﻿// Programa para  pedir la cantidad (int) y un precio unitario (decimal) para calcular el iva (0.15 * subtotal) y el total (subtotal + iva).
+//Capture el  formato inválido y desbordes con mensajes útiles.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== PROGRAMA DE FACTURAS PARA UNA FERRETERÍA ===");
+
+        // bucle encargado de validar la entrada de cantidadArticulos
+        int cantidadArticulos;
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("\nCantidad de artículos: ");
+                cantidadArticulos = int.Parse(Console.ReadLine());
+
+
+                if (cantidadArticulos <= 0)
+                {
+   
+                    Console.WriteLine("Entrada de cantidad inválida. Ingresa un número positivo != 0: ");
+                    continue;
+                }
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException: La entrada no tiene el formato necesario. Usa solo números.");
+                continue;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"OverflowException: La entrada sobrepasa el rango máximo, ingrese un número por debajo de {int.MaxValue}.");
+                continue;
+            }
+
+        }
+        // bucle encargado de validar la entrada de precioUnidad
+        decimal precioUnidad;
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("\nPrecio unitario (C$): ");
+                precioUnidad = decimal.Parse(Console.ReadLine());
+
+
+                if (precioUnidad <= 0)
+                {
+                    Console.WriteLine("Entrada del precio unitario inválida. Ingresa un número positivo != 0: ");
+                    continue;
+                }
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException: La entrada no tiene el formato necesario. Usa solo números.");
+                continue;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"OverflowException: La entrada sobrepasa el rango máximo, ingrese un número por debajo de {int.MaxValue}.");
+                continue;
+            }
+
+        }
+        // bloque de cálculos aritméticos
+        decimal subtotal = precioUnidad * cantidadArticulos;
+        decimal iva = subtotal * 0.15m;
+        decimal total = subtotal + iva;
+
+        // formato factura
+        Console.WriteLine($"\nSUBTOTAL: {subtotal:F2}C$\nIVA: {iva:F2}C$\nTOTAL: {total:F2}C$");
+        
+    }
+}
+
+    
+
+
