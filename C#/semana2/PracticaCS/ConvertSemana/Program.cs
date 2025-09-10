71e2838634131c44ee063ee02d54b505275044b3
Rodolfo Ramírez <rarcorp481@gmail.com>
2025-09-10 09:23:09 -0600

    add ConvertSemana to PracticaCS directory

---
+++ C#/semana2/PracticaCS/ConvertSemana/Program.cs
@@ -0,0 +1,38 @@
+﻿//convertidor de días en semanas y días restantes
+
+using System.Numerics;
+using System.Transactions;
+
+namespace ConvertSemana
+{
+    internal class Program
+    {
+        static void ConvertidorSemanas()
+        {
+            int days, days2;
+            double week;
+            
+
+            Console.WriteLine("Convertidor de días en semanas y días restantes.");
+            while (true)
+            {
+                Console.Write("\nEscribe la cantidad de días que deseas convertir: ");
+                if (int.TryParse(Console.ReadLine(), out days) && days > 0 )
+                {
+                    week = days / 7;
+                    days2 = days % 7;
+                    break;
+                }
+                else Console.WriteLine("Ingresa una cantidad de días válida");
+                
+            }
+            if (days2 == 0) Console.WriteLine($"\n{days} días equivalen a {week:F0} semanas.");
+            else Console.WriteLine($"\n{days} días equivalen a {week:F0} semanas y {days2} días.");
+
+        }
+        static void Main(string[] args)
+        {
+            ConvertidorSemanas();
+        }
+    }
+}
