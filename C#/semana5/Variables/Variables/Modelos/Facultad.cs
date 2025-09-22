d00efdaf0e6ce1895749d2729b59bbfaada1ca31
robertofabiot <rftercerom@uamv.edu.ni>
2025-09-22 07:30:25 -0600

    Se creó la clase facultad satisfactoriamente

---
+++ C#/semana5/Variables/Variables/Modelos/Facultad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Variables.Modelos
+{
+    public class Facultad
+    {
+        public String Nombre;
+        public String Codigo;
+
+        public String MostrarDatos()
+        {
+            return "Facultad" + Nombre + ", Código" + Codigo;
+        }
+
+    }
+}
